33a7f439dffaed349a4c145f5c1a9876c038af1c
RShigaki <shigaki.ryusei@gmail.com>
2023-04-25 22:55:21 +0900

    完成

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,63 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function Modal({ onSetNamed, onClosed }: { onSetNamed: (val: string) => void, onClosed: () => void, }) {
+  const ref = useRef<HTMLInputElement>(null);
+
+  const onSaveButtonClicked = () => {
+    if (!ref.current) return;
+
+    const { value } = ref.current;
+    onSetNamed(value);
+  };
+  return (<>
+    <div style={{
+      top: 0,
+      left: 0,
+      position: "fixed",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      width: "100vw",
+      height: "100vh",
+    }}>
+      <div style={{
+        width: "300px",
+        height: "300px",
+        backgroundColor: "lightyellow",
+        justifyContent: "center",
+        alignItems: "center",
+        display: "flex",
+        flexDirection: "column",
+      }}>
+        <p>こんにちは！</p>
+        <input ref={ref} />
+        <button onClick={onSaveButtonClicked}>保存</button>
+        <button onClick={onClosed}>閉じる</button>
+      </div>
+    </div>
+  </>);
+}
+
 function App() {
+  const [name, setName] = useState<string>("");
+  const [isModalOpened, setIsModalOpened] = useState<Boolean>(false);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+          Hello {name}!
+        </p>
+        <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={() => setIsModalOpened(true)}>モーダルウィンドウを開く</button>
       </header>
+      {(isModalOpened && <Modal onSetNamed={(name) => setName(name)} onClosed={() => setIsModalOpened(false)} />)}
+
     </div>
   );
 }
